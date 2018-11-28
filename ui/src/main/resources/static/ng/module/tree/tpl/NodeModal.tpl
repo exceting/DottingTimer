600dc6c8bc90b8e9fda2b2f58800e18fa09d51f0
sunqinwen <sunqinwen@bilibili.com>
2018-11-29 01:05:27 +0800

    节点详细信息展示

---
+++ ui/src/main/resources/static/ng/module/tree/tpl/NodeModal.tpl
@@ -0,0 +1,15 @@
+<div class="modal fade" style="margin-top:100px; margin-left:200px" id="node_info" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+	    <div class="modal-content">
+            <div class="modal-header" style="border-radius:4px; background-color:#39C5BB">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+            <h4 class="modal-title" style="color:#FFF"></h4>
+        </div>
+    <div class="modal-body">
+
+    </div>
+    </div>
+    </div>
+</div>
--- ui/src/main/resources/static/ng/module/tree/view/SpanInfoView.js
+++ ui/src/main/resources/static/ng/module/tree/view/SpanInfoView.js
@@ -5,18 +5,88 @@ define(function (require, exports, module) {
     seajs.use('./css/module/orgchart/tree/style.css');
     seajs.use('./css/module/orgchart/tree/tree_style.css');
 
-    var Template = require('module/tree/tpl/SpanInfoView.tpl');
+    var mainTemp = require('module/tree/tpl/SpanInfoView.tpl');
+    var modalTemp = require('module/tree/tpl/NodeModal.tpl');
 
     var Span = require('module/tree/model/Span');
     var SpanInfoView = Backbone.View.extend({
-        template: Template,
+        mainTpl: mainTemp,
+        modalTpl: modalTemp,
 
         initialize: function () {
             this.model = new Span();
-            this.$el.append(this.template);
+            this.$el.append(this.mainTpl);
+            this.$el.append(this.modalTpl);
         },
 
-        events: {},
+        events: {
+            'click .node': '_nodeInfo'
+        },
+
+        _nodeInfo: function (event) {
+            var $target = $(event.target);
+            var className = $target.prop("className");
+            if (!className.startsWith("edge")) {
+                var node = JSON.parse($target.parent().find(".node_info").val());
+                this.$el.find('#node_info').modal('show');
+                this.$el.find('.modal-title').text(node.title);
+                // 计算耗时比例
+                var total = node.avg_duration + node.expect;
+                var max_rate = 0;
+                var min_rate = 0;
+                if (node.merge > 1) {
+                    total = total + node.max_duration + node.min_duration;
+                    max_rate = (node.max_duration / total) * 100;
+                    min_rate = (node.min_duration / total) * 100;
+                }
+                var avg_rate = (node.avg_duration / total) * 100;
+                var expect_rate = (node.expect / total) * 100;
+
+                var htm = "<table class=\"table table-striped\"><thead><tr><th style='width: 15rem'>属性</th><th style='width: 85rem'>值</th></tr></thead><tbody>";
+                htm += "<tr><td>链路ID</td><td>TRACE_ID = " + node.trace_id + "; SPAN_ID = " + node.span_id + "</td></tr>";
+                htm += "<tr><td>所属项目</td><td>" + node.moudle + "</td></tr>";
+                htm += "<tr><td>方法全名</td><td>" + node.title + "</td></tr>";
+                htm += "<tr><td>实际耗时</td><td>";
+                htm += this.rateTimeHtm(avg_rate, node.avg_duration);
+                htm += "</td></tr>";
+                htm += "<tr><td>期望耗时</td><td>";
+                htm += this.rateTimeHtm(expect_rate, node.expect);
+                htm += "<tr><td>节点状态</td><td>";
+                if (node.is_error) {
+                    htm += "<span class=\"label label-danger\">异常</span>";
+                } else {
+                    if (node.avg_duration > node.expect) {
+                        htm += "<span class=\"label label-warning\">超出预期</span>"
+                    } else {
+                        htm += "<span class=\"label label-success\">正常</span>"
+                    }
+                }
+                htm += "</td></tr>";
+                if (node.merge > 1) {
+                    htm += "<tr><td>重复执行</td><td>" + node.merge + "次</td></tr>";
+                    htm += "<tr><td>平均耗时</td><td>";
+                    htm += this.rateTimeHtm(avg_rate, node.avg_duration);
+                    htm += "</td></tr>";
+                    htm += "<tr><td>最大耗时</td><td>";
+                    htm += this.rateTimeHtm(max_rate, node.max_duration);
+                    htm += "</td></tr>";
+                    htm += "<tr><td>最小耗时</td><td>";
+                    htm += this.rateTimeHtm(min_rate, node.min_duration);
+                    htm += "</td></tr>";
+                }
+                htm += "<tr><td>其他信息</td><td style='word-wrap:break-word'>"+node.tags+"</td></tr>"
+                htm += "</tbody></table>";
+                this.$el.find('.modal-body').html(htm);
+            }
+        },
+
+        rateTimeHtm: function (rate, time) {
+            if (rate < 8) {
+                return "<div class=\"progress\" style='font-size: 11px; color: #000; font-weight: bolder'>" + time + " ms<div class=\"progress-bar progress-bar-info\" role=\"progressbar\" style=\"width: " + rate + "%;\"></div></div>";
+            } else {
+                return "<div class=\"progress\"><div class=\"progress-bar progress-bar-info\" role=\"progressbar\" style=\"width: " + rate + "%; padding-left: 10px; text-align: left;font-weight: bolder\">" + time + "ms</div></div>";
+            }
+        },
 
         request: function (id) {
             if (id == null || id == undefined || id == "") {
@@ -61,7 +131,7 @@ define(function (require, exports, module) {
             if (treeNode != null && treeNode.node != null) {
                 tree_child.name = treeNode.node.short_title;
                 tree_child.title =
-                    "实际耗时：<span style='color: #e72f6a; font-weight: bold'>" + treeNode.node.avg_duration + "ms</span><br/>"
+                    "<input class='node_info' type='hidden' value='" + JSON.stringify(treeNode.node) + "'/>实际耗时：<span style='color: #e72f6a; font-weight: bold'>" + treeNode.node.avg_duration + "ms</span><br/>"
                     + "期望耗时：<span style='color: #00a5ff; font-weight: bold'>" + treeNode.node.expect + "ms</span><br/>";
                 if (treeNode.node.merge > 1) {
                     tree_child.title += "重复执行：<span class=\"badge\" style=\"background-color:#ff68b5\">" + treeNode.node.merge + "次</span><br/>"
